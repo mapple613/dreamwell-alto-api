ad42f7b63bf538bbc9cbb61c125ea50a274c33f9
mapple613 <info@dreamwell.uk>
2025-06-23 17:09:16 +0100

    Add files via upload

---
+++ properties.php
@@ -0,0 +1,27 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$username = "DreamW052BHxml";
+$password = "hZRGf0B0N2G9mOy";
+$datafeedID = "DreamwellAPI";
+
+$url = "https://api.alto.vebra.com/export/$datafeedID/v10/property";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ["Accept: application/json"]);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+if (curl_errno($ch)) {
+    echo "CURL error: " . curl_error($ch);
+} else {
+    echo "HTTP status: $httpCode<br><br>";
+    echo "<pre>" . htmlspecialchars($response) . "</pre>";
+}
+curl_close($ch);
+?>
